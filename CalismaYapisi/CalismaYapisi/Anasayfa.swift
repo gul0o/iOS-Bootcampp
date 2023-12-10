52b27e718555ea09912eb5c5c2949154ffde781a
Gülseren Doğrul <126578551+gul0o@users.noreply.github.com>
2023-12-10 12:57:32 +0300

    Add files via upload

---
+++ CalismaYapisi/CalismaYapisi/Anasayfa.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalismaYapisi
+//
+//  Created by Gülseren Doğrul on 4.10.2023.
+//
+
+import UIKit
+
+class Anasayfa: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
